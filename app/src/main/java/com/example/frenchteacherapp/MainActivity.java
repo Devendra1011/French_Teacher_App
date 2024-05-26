e0c213ddd4dd73af2648a19b867218f5c0ad9de4
Devendra Raj Bhatta <bhattadevendra60@gmail.com>
2024-05-26 07:14:30 +0545

    final touch

--- app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.frenchteacherapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btnGreen,btnYellow,btnRed,btnPurple,btnBlack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        btnBlack = findViewById(R.id.btn_black);
+        btnGreen = findViewById(R.id.btn_green);
+        btnPurple = findViewById(R.id.btn_purple);
+        btnYellow = findViewById(R.id.btn_yellow);
+        btnRed = findViewById(R.id.btn_red);
+
+
+        btnRed.setOnClickListener(this);
+        btnGreen.setOnClickListener(this);
+        btnPurple.setOnClickListener(this);
+        btnBlack.setOnClickListener(this);
+        btnYellow.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int clickedBtnId = v.getId();
+
+        if (clickedBtnId == R.id.btn_red){
+         playSound(R.raw.red);
+        } else if(clickedBtnId == R.id.btn_black){
+            playSound(R.raw.black);
+        }else if(clickedBtnId == R.id.btn_green){
+            playSound(R.raw.green);
+        } else if(clickedBtnId == R.id.btn_purple){
+            playSound(R.raw.purple);
+        }else {
+            playSound(R.raw.yellow);
+        }
+
+    }
+
+    public void playSound(int id){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,id);
+        mediaPlayer.start();
     }
 }
